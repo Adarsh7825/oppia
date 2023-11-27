055a900791a52b007493de34469af1c584f61186
Adarsh Gupta <csedsadarsh@gmail.com>
2023-11-27 17:53:37 +0530

    optional chaining

--- core/templates/domain/classroom/classroom-backend-api.service.ts
+++ core/templates/domain/classroom/classroom-backend-api.service.ts
@@ -127,7 +127,7 @@ export class ClassroomBackendApiService {
       }
     }, errorResponse => {
       if (errorCallback) {
-        errorCallback(errorResponse.error.error);
+        errorCallback(errorResponse?.error?.error || 'An unknown error occurred');
       }
     });
   }
@@ -150,7 +150,7 @@ export class ClassroomBackendApiService {
         '/new_classroom_id_handler').toPromise().then(response => {
         resolve(response.classroom_id);
       }, errorResponse => {
-        reject(errorResponse.error.error);
+        reject(errorResponse?.error?.error || 'An unknown error occurred');
       });
     });
   }
@@ -178,7 +178,7 @@ export class ClassroomBackendApiService {
           }
         });
       }, errorResponse => {
-        reject(errorResponse.error.error);
+        reject(errorResponse?.error?.error || 'An unknown error occurred');
       });
     });
   }
@@ -197,7 +197,7 @@ export class ClassroomBackendApiService {
           response => {
             resolve(response);
           }, errorResponse => {
-            reject(errorResponse.error.error);
+            reject(errorResponse?.error?.error || 'An unknown error occurred');
           });
     });
   }
@@ -213,7 +213,7 @@ export class ClassroomBackendApiService {
         response => {
           resolve(response);
         }, errorResponse => {
-          reject(errorResponse.error.error);
+          reject(errorResponse?.error?.error || 'An unknown error occurred');
         }
       );
     });
@@ -226,7 +226,7 @@ export class ClassroomBackendApiService {
         '/classroom_admin_data_handler').toPromise().then(response => {
         resolve(response.classroom_id_to_classroom_name);
       }, errorResponse => {
-        reject(errorResponse.error.error);
+        reject(errorResponse?.error?.error || 'An unknown error occurred');
       });
     });
   }
@@ -269,7 +269,7 @@ export class ClassroomBackendApiService {
         classroomIdUrl).toPromise().then(response => {
         resolve(response.classroom_id);
       }, errorResponse => {
-        reject(errorResponse.error.error);
+        reject(errorResponse?.error?.error || 'An unknown error occurred');
       });
     });
   }
