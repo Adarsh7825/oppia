767289a1b462bb730a608ae6fd051a042097eb3b
Adarsh Gupta <csedsadarsh@gmail.com>
2023-12-12 20:36:04 +0530

    added strict types

--- core/domain/state_domain.py
+++ core/domain/state_domain.py
@@ -1545,28 +1545,15 @@ class InteractionInstance(translation_domain.BaseTranslatableObject):
                 selections.
             ValidationError. Answer choices are empty or duplicate.
         """
-        # Here we use cast because we are narrowing down the type from
-        # various allowed cust. arg types to 'int', and here we are sure
-        # that the type is always going to be int because 'ItemInputSelection'
-        # customization args always contains 'minAllowableSelectionCount' key
-        # with int type of values.
         min_value = self.customization_args['minAllowableSelectionCount'].value
-
-        # Here we use cast because we are narrowing down the type from
-        # various allowed cust. arg types to 'int', and here we are sure
-        # that the type is always going to be int because 'ItemInputSelection'
-        # customization args always contains 'maxAllowableSelectionCount' key
-        # with int type of values.
         max_value = self.customization_args['maxAllowableSelectionCount'].value
 
         if min_value is not None and max_value is not None:
-            min_value = cast(int, min_value) # pylint: disable=useless-cast
-            max_value = cast(int, max_value) # pylint: disable=useless-cast
+        
+            min_value = cast(int, min_value)
+            max_value = cast(int, max_value)
         else:
-        # Raises:
-        #     ValueError. If minAllowableSelectionCount or maxAllowableSelectionCount is None.
-            raise
-            ValueError('minAllowableSelectionCount or maxAllowableSelectionCount is None')
+            raise ValueError('minAllowableSelectionCount or maxAllowableSelectionCount is None')
 
         rule_spec_till_now: List[RuleSpecDict] = []
 
